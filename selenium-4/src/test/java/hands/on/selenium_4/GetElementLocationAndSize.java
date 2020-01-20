d4ae72edc1e3051a3f65a3fbe87f1f034a9f8bbe
gaurav.khanna <gaurav.khanna@10.83.14.203>
2020-01-20 17:52:09 +0530

    New Changes

--- selenium-4/src/test/java/hands/on/selenium_4/GetElementLocationAndSize.java
+++ selenium-4/src/test/java/hands/on/selenium_4/GetElementLocationAndSize.java
@@ -1,41 +1,41 @@
 package hands.on.selenium_4;
 
-import java.io.File;
 import java.io.IOException;
 
-import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
-import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class CaptureScreenshotOfWebElement {
-	
+public class GetElementLocationAndSize {
+
 	@Test
 	public void example1() throws IOException {
-		
+
 		WebDriverManager.chromedriver().setup();
-		
+
 		WebDriver driver = new ChromeDriver();
-		
+
 		driver.get("https://selenium.dev/documentation/en/");
-		
+
 		driver.manage().window().maximize();
-		
+
 		WebElement headerText = driver.findElement(By.id("the-selenium-browser-automation-project"));
-		
-		File source = headerText.getScreenshotAs(OutputType.FILE);
-		
-		File destination = new File("logo.png");
-		
-		FileUtils.copyFile(source, destination);
-		
+
+		System.out.println("Height is " + headerText.getRect().getDimension().getHeight());
+
+		System.out.println("Width is " + headerText.getRect().getDimension().getWidth());
+
+		System.out.println("Location X is " + headerText.getRect().getX());
+
+		System.out.println("Location Y is " + headerText.getRect().getY());
+
 		driver.quit();
-		
+
 	}
 
 }
---
+++ selenium-4/src/test/java/hands/on/selenium_4/OpeningNewWindow.java
@@ -0,0 +1,31 @@
+package hands.on.selenium_4;
+
+import java.io.IOException;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class OpeningNewWindow {
+
+	@Test
+	public void example1() throws IOException {
+
+		WebDriverManager.chromedriver().setup();
+
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("https://selenium.dev/documentation/en/");
+
+		driver.switchTo().newWindow(WindowType.WINDOW);
+
+		driver.get("https://www.google.com");
+
+		driver.quit();
+
+	}
+
+}
